7b7aa7dddd04be376941aa3e95c935204f5ed9fb
Zachary Charlop-Powers <zcpowers@Zacharys-MacBook-Air.local>
2022-10-01 14:55:58 -0400

    test templatefile

---
+++ build.clj
@@ -0,0 +1,12 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
+            [org.corfield.build :as bb]))
+
+(def lib 'net.clojars.zachcp/ghclerk)
+(def version "0.1.0-SNAPSHOT")
+#_ ; alternatively, use MAJOR.MINOR.COMMITS:
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
